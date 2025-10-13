ff175f9148112f83dd1341c923c042b602e6d29a
Thasbeeh <ahmedthasbeeh@gmail.com>
2025-10-14 01:01:14 +0400

    Remove unwanted scope

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,11 +6,6 @@ class Conversation < ApplicationRecord
   validates :user1_id, uniqueness: { scope: :user2_id, message: "conversation already exists with this user" }
   validate :user1_id_less_than_user2_id
 
-  scope :between_users, ->(user1_id, user2_id) {
-    user_ids = [ user1_id, user2_id ].sort
-    where("user1_id = ? AND user2_id = ?", user_ids.first, user_ids.second)
-  }
-
   private
 
   def user1_id_less_than_user2_id
