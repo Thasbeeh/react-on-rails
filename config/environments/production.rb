0f330c00bf0d60647d1d5cd7b8f9513deccfe410
Thasbeeh <ahmedthasbeeh@gmail.com>
2025-10-13 14:08:11 +0400

    production origin whitelisting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_cable.allowed_request_origins = [
+    "https://react-on-rails-7y0y.onrender.com"
+  ]
 end
