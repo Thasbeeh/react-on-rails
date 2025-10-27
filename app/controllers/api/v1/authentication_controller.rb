acf42598949ebc402202b6a855b9140e82f8ea17
Thasbeeh <ahmedthasbeeh@gmail.com>
2025-10-27 23:55:57 +0400

    Add Login/SignUp validations

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -3,14 +3,14 @@ class Api::V1::AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
-    @user = User.find_by(username: params[:user][:username])
+    @user = User.find_by(email: params[:user][:email])
     if @user&.authenticate(params[:user][:password])
       token = jwt_encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                      user: @user.slice(:id, :username, :email) }, status: :ok
     else
-      render json: { error: "unauthorized" }, status: :unauthorized
+      render json: { error: "Login failed. Please check your credentials." }, status: :unauthorized
     end
   end
 end
